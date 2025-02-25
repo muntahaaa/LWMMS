04a6f4114a241fea929cc7a19b9860c281b17989
adnan-bin-wahid <adnan.iit.du@gmail.com>
2025-02-25 07:57:27 +0600

    Added new file in featureByAdnan

--- backend/src/utils/catchAsync.js
+++
@@ -1,8 +0,0 @@
-const catchAsync = (fn) => {
-    const errorHandler= (req, res, next) => {
-      fn(req, res, next).catch(next);
-    };
-    return errorHandler;
-  }
-  
-  module.exports = catchAsync;
