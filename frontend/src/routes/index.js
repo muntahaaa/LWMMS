c14ac5ff7d256274673bf83a7cd394018d7c320e
adnan-bin-wahid <adnan.iit.du@gmail.com>
2025-02-28 22:41:07 +0600

    Log In reg

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,7 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
 import Home from '../pages/Home'
-
+import Login from '../pages/Login'
+import ForgotPassowrd from '../pages/ForgotPassowrd'
+import SignUp from '../pages/SignUp'
+import AdminPanel from '../pages/AdminPanel'
+import AllUsers from '../pages/AllUsers'
+import AllProducts from '../pages/AllProducts'
+import CategoryProduct from '../pages/CategoryProduct'
+import ProductDetails from '../pages/ProductDetails'
+import Cart from '../pages/Cart'
+import SearchProduct from '../pages/SearchProduct'
 
 const router = createBrowserRouter([
     {
@@ -11,9 +20,50 @@ const router = createBrowserRouter([
             {
                 path : "",
                 element : <Home/>
-            }
+            },
+            {
+                path : "login",
+                element : <Login/>
+            },
+            {
+                path : "forgot-password",
+                element : <ForgotPassowrd/>
+            },
+            {
+                path : "sign-up",
+                element : <SignUp/>
+            },
+            {
+                path : "product-category",
+                element : <CategoryProduct/>
+            },
+            {
+                path : "product/:id",
+                element : <ProductDetails/>
+            },
+            {
+                path : 'cart',
+                element : <Cart/>
+            },
+            {
+                path : "search",
+                element : <SearchProduct/>
+            },
+            {
+                path : "admin-panel",
+                element : <AdminPanel/>,
+                children : [
+                    {
+                        path : "all-users",
+                        element : <AllUsers/>
+                    },
+                    {
+                        path : "all-products",
+                        element : <AllProducts/>
+                    }
+                ]
+            },
         ]
-        
     }
 ])
 
